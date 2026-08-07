7f38b97ee4f0afe256e9700b2182aed459b45f88
Bo Ji <jibobo1017@gmail.com>
2026-08-07 16:06:53 -0700

    add lidar and image loading processing in data loader

--- include/DataLoader.h
+++ include/DataLoader.h
@@ -3,29 +3,32 @@
 //
 #pragma once
 #include <map>
-#include <General.h>
+#include "General.h"
 #include "DataType.h"
 #include <nlohmann/json.hpp>
 
 class DataLoader{
 public:
- DataLoader(Config& config): config_(config){};
+ DataLoader(Config& config): config_(config){
+    load_lidar();
+ };
  Status load();
  std::map<int, Camera>& get_camera_map();
+ std::map<int, LidarPointCloudInfo>& get_lidar_info_map();
 private:
  Config config_;
  CameraIntrinsic intrinsic_;
  CameraExtrinsic extrinsic_;
  std::map<int, Camera>camera_map_;
+ std::map<int, LidarPointCloudInfo>lidar_info_map_;
  CameraIntrinsic load_intrinsic();
  CameraExtrinsic load_extrinsic();
  void load_depth();
  void load_camera();
-
+ void load_lidar();
 //helper
- Mat4d load_matrix4d(
-    const nlohmann::json& json_matrix);
-
-
+ Mat4d load_matrix4d(const nlohmann::json& json_matrix);
+ void read_ply_xyz(const std::string& filename , std::vector<Vec3d>& points);
+ void load_keyframe_jsonl();
 
 };
--- src/DataLoader.cpp
+++ src/DataLoader.cpp
@@ -27,6 +27,10 @@ std::map<int, Camera>& DataLoader::get_camera_map(){
  return camera_map_;
 }
 
+std::map<int, LidarPointCloudInfo>& DataLoader::get_lidar_info_map(){
+ return lidar_info_map_;
+}
+
 CameraIntrinsic DataLoader::load_intrinsic(){
   std::string intrinsic_path  = config_.camera_intrinsic_path_;
      std::ifstream fin(intrinsic_path);
@@ -116,8 +120,7 @@ CameraExtrinsic DataLoader::load_extrinsic(){
 void DataLoader::load_depth(){
      int loaded_count = 0;
      int missing_count = 0;
-  const fs::path depth_dir =
-       config_.projective_z_buffer_dir_;
+     const fs::path depth_dir = config_.projective_z_buffer_dir_;
 
      if (!fs::exists(depth_dir)) {
          throw std::runtime_error(
@@ -221,9 +224,49 @@ void DataLoader::load_depth(){
 }
 
 void DataLoader::load_camera(){
-  std::string camera_path = config_.input_img_dir_;
-     //Todo:: add implementation
+  std::string camera_img_dir = config_.input_img_dir_;
+  std::string camera_timestamp_path = config_.camera_timestamp_path_;
+  std::ifstream file(camera_timestamp_path);
+  if (!file.is_open()) {
+        throw std::runtime_error(
+            "Failed to open camera timestamp file: " +
+            config_.camera_timestamp_path_);
+    }
+    camera_map_.clear();
+
+    std::string image_name;
+    double timestamp;
+    int camera_id = 0;
+
+    while (file >> image_name >> timestamp) {
+
+        Camera camera;
+        camera.camera_id_ = camera_id;
+        camera.camera_name_ = image_name;
+        camera.time_stamp_ = timestamp;
+
+       // Full image path
+        camera.camera_path_ = camera_img_dir + "/" + image_name;
+        // Initial pose will be filled later
+        camera.initial_T_wc_ = SE3d();
+        camera_map_[camera_id] = camera;
+        ++camera_id;
+    }
+    file.close();
+    std::cout << "Loaded " << camera_map_.size() << " camera images\n";
   return;
+}
+
+void DataLoader::load_lidar(){
+    // load keyframes from json file
+    load_keyframe_jsonl();
+
+    // load the lidar point for each key frames [temporary disable for performance/memeroy concern]
+    // for(auto& [lidar_id, info] : lidar_info_map_){
+    //     lidar_info_map_[lidar_id].points_.clear();
+    //     read_ply_xyz(lidar_info_map_[lidar_id].lidar_path_, lidar_info_map_[lidar_id].points_);
+    // }
+    return;
 }
 
 Mat4d DataLoader::load_matrix4d(
@@ -250,4 +293,97 @@ Mat4d DataLoader::load_matrix4d(
          }
      }
      return matrix;
+}
+
+void DataLoader::read_ply_xyz(const std::string& filename , std::vector<Vec3d>& points){
+
+    std::ifstream file(filename);
+
+    if(!file.is_open()){
+        throw std::runtime_error("Cannot open ply file: " + filename);
+    }
+
+    std::string line;
+    size_t vertex_count = 0;
+    while(std::getline(file, line)){
+        std::stringstream ss(line);
+        std::string key;
+        ss >> key;
+        if(key == "element"){
+            std::string type;
+            ss >> type;
+
+            if(type == "vertex"){
+                ss >> vertex_count;
+            }
+        }
+        if(line == "end_header"){
+            break;
+        }
+    }
+
+    points.clear();
+    points.reserve(vertex_count);
+    for(size_t i=0; i<vertex_count; i++){
+        double x,y,z;
+        file >> x >> y >> z;
+        points.emplace_back(x,y,z);
+    }
+    file.close();
+}
+
+
+void DataLoader::load_keyframe_jsonl(){
+    // Load the keyframe JSONL file specified in the configuration
+    std::ifstream file(config_.lidar_kf_path_);
+    if(!file.is_open()){
+        throw std::runtime_error("Cannot open keyframe JSONL file: " + config_.lidar_kf_path_);
+    }
+    std::string line;
+    std::string json_buffer;
+    int brace_count = 0;
+    while(std::getline(file, line)){
+        if (line.empty())
+        continue;
+        // Count opening/closing braces
+        for (char c : line) {
+            if (c == '{')
+                ++brace_count;
+            else if (c == '}')
+                --brace_count;
+        }
+        json_buffer += line;
+        json_buffer += '\n';
+        if (brace_count == 0 && !json_buffer.empty()) {
+        // Process the accumulated JSON buffer as a JSON object
+            nlohmann::json json_line = nlohmann::json::parse(json_buffer);
+            // Handle the JSON object as needed
+            LidarPointCloudInfo info;
+
+            info.lidar_id_ = json_line["key_frame_id"].get<int>();
+            info.time_stamp_ = json_line["timestamp"].get<double>();
+            info.lidar_path_ = config_.lidar_ply_dirs_+ json_line["saved_frame_path"].get<std::string>().erase(0,1);
+            std::cout<<info.lidar_path_<<"\n";
+            // get the LIO se3d
+            auto t = json_line["lio_pose"]["translation"];
+            Eigen::Vector3d translation(
+                t[0].get<double>(),
+                t[1].get<double>(),
+                t[2].get<double>());
+            auto q = json_line["lio_pose"]["quaternion_xyzw"];
+            Eigen::Quaterniond rotation(
+                q[3].get<double>(), // w
+                q[0].get<double>(), // x
+                q[1].get<double>(), // y
+                q[2].get<double>()  // z
+            );
+            info.initial_T_wl_ = SE3d(rotation, translation);
+            lidar_info_map_[info.lidar_id_] = info;
+            json_buffer.clear();
+        }
+    }
+
+    file.close();
+    std::cout<< "Finished loading keyframe JSONL. Total keyframes: " << lidar_info_map_.size() << std::endl;
+
 }
